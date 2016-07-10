ed3382d18f4c44a96c2579e40cc1eac8cd482e52
SimonDieck <simon_dieck@web.de>
2016-07-10 19:42:04 +0000

    IO functions for actions and procedures
    Added most of the core IO functionality, need to add IO functions for the actions that can trigger other actions but most of them should be done in very few lines.
    A bit behind on my time estimation but should still be easily possible to finish in time.

--- Mtgmodel.hs
+++ Mtgmodel.hs
@@ -25,13 +25,13 @@ newtype Stack = Stack [Card]
 
 newtype Life = Life Int
 
-data Zone = ZLibrary | ZGraveyard | ZBattlefield | ZHand | ZExile
+data Zone = ZLibrary | ZGraveyard | ZBattlefield | ZHand | ZExile deriving (Eq)
 
 data Side = Side Player Library Graveyard Battlefield Hand Exile Manapool Life
 
 
 --Gameprocedure related
-data Phase = UntapPhase | Upkeep | Draw | FirstMain | DeclareAttackers | DeclareBlockers | ResolveCombat | SecondMain | Endstep deriving (Enum, Bounded)
+data Phase = UntapPhase | Upkeep | Draw | FirstMain | DeclareAttackers | DeclareBlockers | ResolveCombat | SecondMain | Endstep deriving (Enum, Bounded, Eq)
 
 newtype Player = Id Int deriving (Eq)
 
@@ -53,17 +53,43 @@ newtype PowerToughness = PT (Maybe (Int, Int))
 data Keyword = Deathtouch | Defender | DoubleStrike | FirstStrike | Flash | Flying | Haste | Hexproof | Indestructible | Lifelink | Menace | Reach | Trample | Vigilance | Fear | Shroud | Intimidate
 
 --Possible events that can be asked by triggers. Listed undet Actions at: http://mtgsalvation.gamepedia.com/Evergreen
-data Event = Activate | Attach | Cast | Counter | Destroy | Discard | Exchange | ExileCard | Fight | Play | Regenerate | Reveal | Sacrifice | Scry | Search | Shuffle | Tap | Untap
+data Event = Activate | Attach | Cast | Counter | Destroy | Discard | Exchange | ExileCard | Fight | Play | Regenerate | Reveal | Sacrifice | Scry | Search | Shuffle | Tap | Untap deriving (Eq)
 
 data Change = Change CardId (Card -> Card)
 
 --               Id     Name     Type       Subtype   Power/ Toughness  Cost     Additional Cost  Effect Owner   Damage Untapped = True  Active Changes affecting the card  Keywords
-data Card = Card CardId Cardname [Cardtype] [Subtype] PowerToughness    Manacost Cost             Effect Player  Int    Bool             [Change]                           [Keyword]  Trigger
+data Card = Card CardId Cardname [Cardtype] [Subtype] PowerToughness    Manacost Cost             Effect Player  Int    Bool             [Change]                           [Keyword]  [Trigger]
 
 
 --Gamestate
---                         All Sides  Player whose Turn it is  Current Phase  Player whose Priority it is
-data Gamestate = Gamestate [Side]     Player                   Phase          Player
+--                         All Sides  Player whose Turn it is  Current Phase  Player whose Priority it is  List of all Players
+data Gamestate = Gamestate [Side]     Player                   Phase          Player                       [Player]
+
+setPhaseTo :: Phase -> Gamestate -> Gamestate
+setPhaseTo phs (Gamestate s p ph pl all) = Gamestate s p phs pl all
+
+advancePhase :: Gamestate -> Gamestate
+advancePhase (Gamestate s p ph pl all) = let g = (Gamestate s p ph pl all) in if ph == maxBound then nextTurn g else (Gamestate s p (succ ph) pl all)
+
+nextTurn :: Gamestate -> Gamestate
+nextTurn (Gamestate s p ph pl all) = let x = findNext p all in (Gamestate s x minBound x all)
+
+changePriority :: Gamestate -> Gamestate
+changePriority (Gamestate s p ph pl all) = let x = findNext pl all in (Gamestate s p minBound x all)
+
+findNext :: Eq a => a -> [a] -> a
+findNext x y = case maybefindNext x y of
+                    Just b  -> b
+                    Nothing -> head y
+                    
+maybefindNext :: Eq a => a -> [a] -> Maybe a
+maybefindNext _ [a] = Nothing
+maybefindNext x (y :ys) = if x == y then Just (head ys) else maybefindNext x ys
+
+
+--currently a stub repeats endlessly
+checkWinner :: Gamestate -> Bool
+checkWinner _ = False
 
 
 --Cardeffects
@@ -73,13 +99,16 @@ data Resource = RLibrary | RGraveyard | RBattlefield | RHand | RExile | RStack |
 
 data Cost = Cost (Side -> Bool) [(Side -> Side)]
 
---Actions have IDs so Triggers can be called
-data Action = Action (Side -> Side) Player | Procedure
+data Action = Action Target Event (Maybe Int) (Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure)
 
---                      Trigger        Consequence OneTime or Permanent Origin
-data Trigger = Trigger  Event          Action      Bool                 CardId
+--                      Trigger        Consequence OneTime or Permanent Origin Zone in which the Trigger will activate
+data Trigger = Trigger  Event          Action      Bool                 CardId Zone
 
-type Procedure = (Gamestate -> Gamestate)
+newtype Procedure = Procedure (Gamestate -> IO Gamestate)
+
+data Target = Self | Opponent | ConditionalTarget (Card -> Bool) (Zone -> Bool) (Player -> Bool) | AllPlayer | ConditionalAllCards (Card -> Bool) (Zone -> Bool) (Player -> Bool)
+
+data Source = CardSource CardId | PlayerInitiator Player
 
 --Replaces a procedure with an alternative. Boolean checks if this is done once or as long as the card persists
 data AltProcedure = Alternative ((Gamestate -> Gamestate), Int) Bool   CardId
@@ -98,8 +127,39 @@ checkCard :: CardId -> Card -> Bool
 checkCard i (Card id _ _ _ _ _ _ _ _ _ _ _ _ _) = id == i 
 
 
+checkTrigger :: Event -> Zone -> Trigger -> Bool
+checkTrigger e z (Trigger e' _ _ _ z') = e == e' && z == z'
+
+
+affectedTriggers :: (Trigger -> Bool) -> Card -> [Trigger]
+affectedTriggers scp (Card _ _ _ _ _ _ _ _ _ _ _ _ _ tr) = filter scp tr
+
+
+affectedTriggersZone :: (Trigger -> Bool) -> [Card] -> [Trigger]
+affectedTriggersZone scp s = concatMap (affectedTriggers scp) s
+
+
+dealDamage :: Int -> Card -> Card
+dealDamage x (Card id n tp s pt c ac e o d t chng k tr) = Card id n tp s pt c ac e o (d+x) t chng k tr
+
+
+checkLethal :: Card -> Bool
+checkLethal (Card _ _ _ _ (PT Nothing) _ _ _ _ d _ _ _ _)      = False
+checkLethal (Card _ _ _ _ (PT (Just (_,b))) _ _ _ _ d _ _ _ _) = b <= d
+
+
+allAffectedTriggers :: Event -> Side -> [Trigger]
+allAffectedTriggers ev (Side _ (Lib lib) (Grave g) (Btlf b) (Hand h) (Exile e) _ _) = concatMap (\(i,j) -> affectedTriggersZone (checkTrigger ev i) j) (zip [ZLibrary, ZGraveyard, ZBattlefield, ZHand, ZExile] [lib, g, b, h, e])
+
+
 manipulateResource :: Player -> (Side -> Side) -> Gamestate -> Gamestate
-manipulateResource pl r (Gamestate s t p pr) = Gamestate (map r (filter (ownerSide pl) s)) t p pr
+manipulateResource pl r (Gamestate s t p pr all) = Gamestate (condMap (ownerSide pr) r s) t p pr all
+
+
+condMap :: (a -> Bool) -> (a -> a) -> [a] -> [a]
+condMap c p []       = []
+condMap c p (x : xs) = if c x then ((p x) : (condMap c p xs)) else (x : (condMap c p xs)) 
+
 
 -- important function which will be used with the parser
 changeLife :: Int -> Side -> Side
@@ -169,6 +229,10 @@ checkUntap (Card _ _ _ _ _ _ _ _ _ _ t _ _ _) = t
 
 getCardId :: Card -> CardId
 getCardId (Card id _ _ _ _ _ _ _ _ _ _ _ _ _) = id
+
+
+getOwner :: Card -> Player
+getOwner (Card _ _ _ _ _ _ _ _ p _ _ _ _ _) = p
 
 
 checkType :: Cardtype -> Card -> Bool
@@ -259,9 +323,140 @@ applyAll [] v = v
 applyAll (x : xs) v = applyAll xs (x v)
  
 
+ioapplyAll :: [(a -> IO a)] -> a -> IO a
+ioapplyAll [] v = return v
+ioapplyAll (x : xs) v = do v' <- x v
+                           ioapplyAll xs v'
+                           
+                           
+ioFold :: (a -> b -> IO b) -> b -> [a] -> IO b
+ioFold _ v []       = return v
+ioFold f v (x : xs) = do v' <- f x v
+                         ioFold f v' xs
+                           
+
 payaddCost :: Cost -> Side -> Side
 payaddCost (Cost _ chngs) s = applyAll chngs s
+
+
+findSide :: Gamestate -> Player -> Side
+findSide (Gamestate s _ _ _ _) p' =  head (filter (ownerSide p') s)
+
+
+allZonesSatisfying :: (Zone -> Bool) -> Side -> [Card]
+allZonesSatisfying zn (Side _ (Lib lib) (Grave g) (Btlf b) (Hand h) (Exile e) _ _)= concatMap (snd) (filter (\(i,j) -> zn i) (zip [ZLibrary, ZGraveyard, ZBattlefield, ZHand, ZExile] [lib, g, b, h, e]))
+
+
+allCardsSatisfying :: (Card -> Bool) -> (Zone -> Bool) -> (Player -> Bool) -> [Side] -> [CardId]
+allCardsSatisfying c zn o s = map getCardId (filter c (concatMap (allZonesSatisfying zn) (filter (\(Side pl _ _ _ _ _ _ _) -> o pl) s)))
 
 --Main method for paying costs but still need binding functions for casting and activating abilities which will also check the cost and afterwards apply all effects
 payCost :: Manacost -> Cost -> Side -> Side
 payCost mc c s = let Side pl lib g b h e m l = payaddCost c s in Side pl lib g b h e (payManaCost mc m) l
+
+
+
+--Block of IO functions modelling the standard interactions and procedures of the game
+findTarget :: Gamestate -> Target -> IO (Either [CardId] [Player])
+findTarget (Gamestate s p ph pl all) t = case t of
+                                          Self                        -> return (Right [pl])
+                                          Opponent                    -> do x <- chooseOpponent pl all
+                                                                            return (Right [x])
+                                          ConditionalTarget c' zn' o' -> do y <- chooseCard c' zn' o' s
+                                                                            return (Left [y])
+                                          AllPlayer                   -> return (Right all)
+                                          ConditionalAllCards c zn o  -> return (Left (allCardsSatisfying c zn o s))
+                                          
+                                          
+chooseOpponent :: Player -> [Player] -> IO Player
+chooseOpponent p all = do putStr "Choose Opponent: " 
+                          x <- getLine
+                          case filter ((\p' -> (p' == (parsePlayer x)) && (not (p' == p)))) all of
+                            []  -> do putStr "Invalid Target"
+                                      chooseOpponent p all
+                            [z] -> return z
+                                          
+
+chooseCard :: (Card -> Bool) -> (Zone -> Bool) -> (Player -> Bool) -> [Side] -> IO CardId
+chooseCard c zn o s = do putStr "Choose Card: "
+                         x <- getLine
+                         let crds = allCardsSatisfying c zn o s in
+                             case filter (\c' -> c' == (parseCard x)) crds of
+                                  []  -> do putStr "Invalid Target"
+                                            chooseCard c zn o s
+                                  [z] -> return z
+                             
+
+                                          
+parsePlayer :: String -> Player
+parsePlayer s = undefined
+
+
+parseCard :: String -> CardId
+parseCard c = undefined
+
+
+applyProcedure :: Procedure -> Gamestate -> IO Gamestate
+applyProcedure (Procedure p) g = p g
+           
+
+executeAction :: Source -> Action -> Gamestate -> IO Gamestate
+executeAction s (Action t e str a) (Gamestate s' p ph pl all) = let g = (Gamestate s' p ph pl all) in
+                                                                    do x   <- findTarget g t
+                                                                       let tr = concatMap (allAffectedTriggers e) (map (findSide g) all) in
+                                                                        do g'  <- applyProcedure (a s x str) g
+                                                                           g'' <- ioFold executeTrigger g' tr
+                                                                           return g''
+                                                                 
+                                      
+
+
+executeTrigger :: Trigger -> Gamestate -> IO Gamestate
+executeTrigger t g = undefined
+                                      
+           
+game :: IO ()
+game = do g <- initalize
+          (while (not.checkWinner) turn) g
+          return ()
+
+
+initalize :: IO Gamestate
+initalize = undefined
+          
+while :: (a -> Bool) -> (a -> IO a) -> (a -> IO a)
+while p body = loop
+    where loop x = if p x then do x' <- body x
+                                  loop x'
+                          else return x
+
+turn :: Gamestate -> IO Gamestate
+turn (Gamestate s p ph pl all) = let g = (Gamestate s p ph pl all) in
+                                  ioapplyAll [untapStep, upkeepStep, drawStep, firstMainStep, combatStep, secondMainStep, endStep] g
+                                 
+                                 
+--TODO: change to use execute action           
+untapStep :: Gamestate -> IO Gamestate
+untapStep (Gamestate s p ph pl all) =  let g = (Gamestate s p ph pl all) in
+                                           return (advancePhase (manipulateResource p (affectInZone ZBattlefield allAccept (tapuntap True)) g))
+                                         
+upkeepStep :: Gamestate -> IO Gamestate
+upkeepStep g = undefined
+
+drawStep :: Gamestate -> IO Gamestate
+drawStep g = undefined
+
+firstMainStep :: Gamestate -> IO Gamestate
+firstMainStep g = undefined
+
+combatStep :: Gamestate -> IO Gamestate
+combatStep g = undefined
+
+secondMainStep :: Gamestate -> IO Gamestate
+secondMainStep g = undefined
+
+endStep :: Gamestate -> IO Gamestate
+endStep g = undefined
+
+
+
