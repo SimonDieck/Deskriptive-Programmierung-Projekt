d2a5817177261c359dd08785d1b0039a376a1c97
SimonDieck <simon_dieck@web.de>
2016-07-16 19:34:26 +0200

    added Parsers for most non-Effect fields
    can now parse most non-effect Fields, and added the basic structure to parse spells. Should be able to finish up spells this evening.

--- src/CardParser.hs
+++ src/CardParser.hs
@@ -7,6 +7,25 @@ import Mtgmodel
 import SupportFunctions
 
 
+defaultOwner :: Player
+defaultOwner = Id 401
+
+
+data EffectSet = Set Cost [Trigger] Effect [Keyword]
+
+extractCost :: EffectSet -> Cost
+extractCost (Set c _ _ _) = c
+
+extractTriggers :: EffectSet -> [Trigger]
+extractTriggers (Set _ t _ _) = t
+
+extractEffect :: EffectSet -> Effect
+extractEffect (Set _ _ e _) = e
+
+extractKeywords :: EffectSet -> [Keyword]
+extractKeywords (Set _ _ _ k) = k
+
+
 player :: Parsec String () Player
 player = do p <- trueDigit
             return (Id p)
@@ -15,6 +34,13 @@ player = do p <- trueDigit
 trueDigit :: Parsec String () Int
 trueDigit = do i <- digit
                return (digitToInt i)
+               
+trueInt :: Parsec String () Int
+trueInt = consecutiveInt 0
+
+consecutiveInt :: Int -> Parsec String () Int
+consecutiveInt i = (try (do i' <- trueDigit
+                            consecutiveInt (i*10+i') )) <|> return i 
  
  
 ignoreField :: Parsec String () ()
@@ -52,6 +78,14 @@ learn' s = do x <- ignoreField
               learn s 
 
 
+textChars :: Parsec String () Char
+textChars = alphaNum <|> headOf (string "'") <|> char ',' <|> char '.'
+
+
+headOf :: Parsec String () String -> Parsec String () Char
+headOf s = do x <- s
+              return (head x)
+              
 
 earlyIdentify :: Parsec String () a -> Parsec String () a
 earlyIdentify p = lookAhead (do skipDataTo p
@@ -71,6 +105,11 @@ skipDataTo :: Parsec String () a -> Parsec String () ()
 skipDataTo s = do manyTill anyChar (try (lookAhead s))
                   return ()
 
+                  
+skipDataToResult :: Parsec String () a -> Parsec String () a
+skipDataToResult s = do manyTill anyChar (try (lookAhead s))
+                        s
+                  
 
 dataSep :: Parsec String () a  -> Parsec String () a
 dataSep p = field '"' '"' p
@@ -101,7 +140,15 @@ creature = undefined
 
 
 spell :: Parsec String () Card
-spell = undefined
+spell = do mc <- skipDataToResult manaCostField
+           id <- skipDataToResult cardidFields
+           ef <- skipDataToResult (ruleTextFieldSpell id)
+           tp <- skipDataToResult cardTypeField
+           let tr = extractTriggers ef
+               stdac = extractEffect ef
+               ac = extractCost ef
+               k = extractKeywords ef
+               in return (Card id (nameFromId id) tp [] (PT Nothing) mc ac stdac defaultOwner 0 True [] k tr)
 
 
 permanent :: Parsec String () Card
@@ -124,4 +171,22 @@ cardType = do t <- many1 letter
                 "Artifact"     -> return Artifact
                 "Land"         -> return Land
 
+cardidFields :: Parsec String () CardId
+cardidFields = do dataSetName "multiverseid"
+                  x <- trueInt
+                  char ','
+                  dataSetName "name"
+                  n <- dataSep (many1 textChars)
+                  return (CardId (x,(Cardname n)))
+                  
+                  
+manaCostField :: Parsec String () Manacost
+manaCostField = undefined
 
+
+ruleTextFieldSpell :: CardId -> Parsec String () EffectSet
+ruleTextFieldSpell = undefined
+
+
+
+                  
