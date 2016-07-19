b75db18c7356758af3d6b9aab71924576be638cf
SimonDieck <simon_dieck@web.de>
2016-07-19 12:37:57 +0200

    Added extensive set of comments to code

--- src/Mtgmodel.hs
+++ src/Mtgmodel.hs
@@ -5,12 +5,37 @@ import SupportFunctions
 
 
 --Recources
+{-- 
+Mana is the primary ressource for paying costs in MtG (Magic: The Gathering)
+Every card mandatorily (with one exception in ca. 13 thousand unique cards) has a manacost associated with it.
+This Mana comes in one of five colours and a cards colour is every colour occuring in its Manacost.
+--}
 data Mana = Red | Green | White | Blue | Black | Colourless deriving (Eq, Show)
 
+
+{--
+Storage for active unspend Mana a player has
+--}
 newtype Manapool = Pool [(Int,Mana)]
 
---costs are aved as negative numbers
+
+{--
+Manacost of a card as explained eralier.
+Costs are saved as negative numbers
+--}
 newtype Manacost = MCost [(Int, Mana)] deriving (Show)
+
+
+{--
+The following six Zones are were cards can be. Every player has his own set of these zones.
+The Library is the deck of cards from where cards are drawn from.
+The Hand is where cards are drawn to most cards can only be cast from your hand.
+Battelfield is where all active cards are placed.
+The Graveyard is where used cards are placed.
+Exile has similar functions to the graveyard
+The Stack is were every card is placed when it is played from ones hand. From there it continues on to the Battlefield or the Graveyard. Used for players to interact with cards before they become active.
+--}
+data Zone = ZLibrary | ZGraveyard | ZBattlefield | ZHand | ZExile | ZStack | ZGlobal deriving (Eq)
 
 newtype Library = Lib [Card]
 
@@ -24,46 +49,72 @@ newtype Exile = Exile [Card]
 
 newtype Stack = Stack [Card]
 
+
+{--
+Starts at 20 if it hits 0 a player loses the game. Is also used as ressource to pay fro some spells and effects.
+--}
 newtype Life = Life Int
 
-data Zone = ZLibrary | ZGraveyard | ZBattlefield | ZHand | ZExile | ZStack | ZGlobal deriving (Eq)
+--A player of the game
+newtype Player = Id Int deriving (Eq, Show)
 
+{--
+Used for targeting purposes, so a card knows which players it is supposed to effect.
+You =  Owner of the card
+TOpponent = Not owner of the card
+Each = all Players
+--}
+data PlayerType = You | TOpponent | Each
+
+--Combination of everything before culminates in a side of a player
 data Side = Side Player Library Graveyard Battlefield Hand Exile Stack Manapool Life
 
 
---Gameprocedure related
+--Phases that are progressed through every turn
 data Phase = UntapPhase | Upkeep | Draw | FirstMain | DeclareAttackers | DeclareBlockers | ResolveCombat | SecondMain | Endstep deriving (Enum, Bounded, Eq)
 
-newtype Player = Id Int deriving (Eq, Show)
 
-data PlayerType = You | TOpponent | Each
-
-
--- Card related Identifiers
+--Card related Identifiers
 newtype Cardname = Cardname String deriving (Eq, Show)
 
 newtype CardId = CardId (Int, Cardname) deriving (Eq, Show)
 
+{--
+Cardtypes are split into primary types and subtypes.
+Primary types have certain rules associated with it and come with implicit effects for the card
+Subtypes are used to target only a specific subset of cards or specify the effects implicated by the primary type.
+--}
 data Cardtype = Instant | Sorcery | Creature | Enchantment | Artifact | Planeswalker | Land | Legendary Cardtype | Token deriving (Eq)
 
 data Subtype = Creaturetype | Mountain | Forest | Plains | Island | Swamp | Aura | Equipment | Subtype String deriving (Eq)
 
 newtype Creaturetype = CreatureType String deriving (Eq)
 
+{--
+Creatures have a Power and Toughness value associated with them used for combat. All other cardtypes don't have one, but can be transfromed into creatures. Therefore a Maybevalue was chosen.
+--}
 newtype PowerToughness = PT (Maybe (Int, Int))
 
---all static abilitys that can occur in m2010 and are abilites that cause alternate procedures and can be requested by other cards. Full List here: http://mtgsalvation.gamepedia.com/Evergreen
+--all static abilitys that can occur in m2010 and are abilites that cause alternate procedures and can be requested by other cards. Full List here: http://mtgsalvation.gamepedia.com/Evergreen Also changes in in the state of a card have been included here.
 data Keyword = Keyword String deriving (Eq)
 
 --Possible events that can be asked by triggers. Listed under Actions at: http://mtgsalvation.gamepedia.com/Evergreen
 data Event = Event String deriving (Eq)
 
+{--
+Changes are functions that can revert a change that was previously conducted by an other cards effect. This is necessary as for example static changes to Power and Toughness disappear when teir source changes zones.
+Most obvious and regular use is in changePT.
+--}
 data Change = Change CardId (Card -> Card)
+
 
 --               Id     Name     Type       Subtype   Power/ Toughness  Cost     Additional Cost  Effect Owner   Damage Untapped = True  Active Changes affecting the card  Keywords
 data Card = Card CardId Cardname [Cardtype] [Subtype] PowerToughness    Manacost Cost             Effect Player  Int    Bool             [Change]                           [Keyword]  [Trigger]
 
 
+{--
+used for error handling in Card -> Card functions
+--}
 errorCard :: Card
 errorCard = Card (CardId ((-1), (Cardname "Error"))) (Cardname "Error") [] [] (PT (Nothing)) (MCost []) (Cost allAccept []) (Effect []) (Id (-1)) 0 False [] [] []
 
@@ -78,34 +129,70 @@ checkWinner _ = False
 
 
 --Cardeffects
-data Scope a = Scope (a -> Bool)
+data Scope a = Scope (a -> Bool) -- conclueded this didn't need an extra type so it was never used.
 
-data Resource = RLibrary | RGraveyard | RBattlefield | RHand | RExile | RStack | RManapool | RLife
+data Resource = RLibrary | RGraveyard | RBattlefield | RHand | RExile | RStack | RManapool | RLife --same as with Scope its functionality was replaced by Zones
 
+
+{--
+Used to model additional costs of cards or effects. A card can require you to pay additional non Mana ressources to be cast. The (Side -> Bool) function checks if those ressources are present.
+Every cost like a change in life or discarding cards can be coded as a Side -> Side function, which are applied consecutively after their existence has been verified.
+--}
 data Cost = Cost (Side -> Bool) [(Side -> Side)]
 
+{--
+Together with Trigger the most important part of the model.
+The last part causes a change in Gamestate. The functions used for this can be found in the IO block.
+To know what is affected by the change described in the last function the Target mode is specified in Target.
+As executeAction is an (Gamestate -> IO Gamestate) function itself it can be part of the Procedure so actions can cascade into each other using informations from the previous action.
+Events are names for the actions and can cause triggers who listen for this to fire allowing for an alternative way of actions cascading itno each other, this way information is separate though.
+ChainedAction is a simple way of specifying a sequence of Actions that don't need information from each other without triggers.
+ConditionalActions is a sequence of actions where all actions after the first know the target of the first Action.
+--}
 data Action = Action Target Event (Maybe Int) (Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure) | ChainedAction [Action] | CondAction [Action]
 
+{--
+Desctiprotrs for implicit actions of cards. That every card of a certain cardtype must have.
+--}
 newtype StandardAction = Descriptor String deriving (Eq)
 
---used to active Triggers of certain events
+--If you use this Action you can call all Triggers listening for a certain event.
 eventAction :: Event -> Action
 eventAction e = Action Self e Nothing emptyChange
 
+
+{--
+A Trigger listens for a certain event. If an Action with that Event is applied the Trigger also applies it's own Action, if the thing that caused the event satisfies the (Source -> Bool)-function.
+Certain Triggers are only active in certain zones. This can be used to prevent actions from happening by changing the zone of the card. Used to model Counterspells.
+--}
 --                      Trigger        Consequence OneTime or Permanent Active   Origin Zone in which the Trigger will activate Scope to check if the Source satisfies certain conditions Owner of the Trigger
 data Trigger = Trigger  Event          Action      Bool                 Bool     CardId Zone                                    (Source -> Bool)                                          Player
 
 newtype Procedure = Procedure (Gamestate -> IO Gamestate)
 
+{--
+The Target mode specifies which cards or players are affected by an action.
+Again - Used by Conditional Action to use a target found by the first action.
+Self - Owner of the action
+Opponet - A player other then the owner
+Condtional Target - A player chooses a card which satisfies all three conditions
+AllPlayer - All players are affected
+ConditionalAllCards - All cards satisfying all three conditions are affected
+CondtionalOwnTarget - Replaced by the extension of the third condition in conditionalTarget
+--}
 data Target = Again | Self | Opponent | ConditionalTarget (Card -> Bool) (Zone -> Bool) (Player -> Player -> Bool) | AllPlayer | ConditionalAllCards (Card -> Bool) (Zone -> Bool) (Player -> Player -> Bool) | ConditionalOwnTarget (Card -> Bool) (Zone -> Bool)
 
 data Source = CardSource CardId | PlayerInitiator Player
 
---
+--List of implicit cardtype actions. These can be replaced by more specific conditions. For example a creature with vigilance wouldn't be tapped if it attacked.
 data Effect = Effect [(StandardAction, Action)] 
 
 --standardActions that get executed unless specified otherwise on a card
 
+{--
+Good example for cascading into other actions. For standard attack the woner of the card gets to choose an opponet. The attack function than executes an action, where the choosen opponent can choose a creaute he controls to block the attacker.
+Which in turn calls an action where the attacking creature and the blocking creature deal damage to each other.
+--}
 standardAttack :: Action
 standardAttack = Action Opponent (Event "Attack") Nothing attack
 
@@ -115,6 +202,9 @@ standardDeclareBlockers = Action (ConditionalOwnTarget (checkUntap) (checkZone Z
 
 
 --Functions modelling change in the model
+{--
+This first set of functions couldn't be sorted in a certain category but are used mostly to find more specific data when given a broader set with conditions.
+--}
 checkPlayerType :: PlayerType -> Player -> Player -> Bool
 checkPlayerType pt p p' = case pt of
                                 You       -> p' == p
@@ -170,6 +260,8 @@ allAffectedTriggers :: Source -> Event -> Side -> [Trigger]
 allAffectedTriggers src ev (Side _ (Lib lib) (Grave g) (Btlf b) (Hand h) (Exile e) _ _ _) = concatMap (\(i,j) -> affectedTriggersZone (checkTrigger src ev i) j) (zip [ZGraveyard, ZBattlefield, ZHand, ZExile] [g, b, h, e])
 
 
+
+--secondary helper functions used to facilitate the functionality of later functions.
 addToZone :: Zone -> (Card, Side) -> Side
 addToZone z (c, (Side pl (Lib lib) (Grave g) (Btlf b) (Hand h) (Exile e) (Stack s) m l)) = 
  case z of
@@ -243,6 +335,7 @@ allZonesSatisfying zn (Side _ (Lib lib) (Grave g) (Btlf b) (Hand h) (Exile e) _ 
 allCardsSatisfying :: (Card -> Bool) -> (Zone -> Bool) -> (Player -> Bool) -> [Side] -> [CardId]
 allCardsSatisfying c zn o s = map getCardId (filter c (concatMap (allZonesSatisfying zn) (filter (\(Side pl _ _ _ _ _ _ _ _) -> o pl) s)))
 
+
 {-
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 
@@ -277,6 +370,8 @@ findCardAnywhere id z (Gamestate s _ _ _ _ _ ) = head (filter (\c -> getCardId c
 
 Card -> Bool Functions 
 
+
+Mainly used to find targets for actions.
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -}
 
@@ -316,6 +411,8 @@ checkCard i (Card id _ _ _ _ _ _ _ _ _ _ _ _ _) = id == i
 
 Side -> Bool Functions 
 
+
+Mainly used to find targets for actions.
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -}
 
@@ -344,6 +441,8 @@ checkManaPlayer cst (Side _ _ _ _ _ _ _ m _) = checkManaCost cst m
 
 Card -> Card Functions 
 
+
+changing specific values of a card used by actions
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -}
 
@@ -386,6 +485,7 @@ removeKeyword kw (Card id n tp s pt c ac e o d t chng k tr) = (Card id n tp s pt
 
 Side -> Side Functions 
 
+changing either resource values like the Manapool or life. Or changing the Zone of a specific card / set of cards
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -}
 
@@ -443,6 +543,7 @@ removeTrigger (Trigger e _ _ _ i z _ o) s = affectInZone z (\c -> (getCardId c) 
 
 Gamestate -> Gamestate Functions 
 
+manipulating the broader aspects of the game like whose turn it is and who currently is allowed to take action. etc.
 |||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||||
 -}
 
@@ -571,7 +672,7 @@ emptyChange :: (Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 emptyChange _ _ _ = Procedure noChange
                                       
 
---add Path via Stack as well as removal after Resolving
+--checks if costs can be payed and then puts the card onto the stack, giving via a global or local trigger waiting for the cast action other players the chance to remove the card from the stack therefore preventing the Resolve trigger of a card from applying
 cast :: Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 cast (PlayerInitiator p) (Left (c:cs)) Nothing = Procedure (\g -> let s = findSide g p 
                                                                       (Card id _ _ _ _ mc (Cost chk cst) _ _ _ _ _ _ _) = findCard c ZHand s in
@@ -584,9 +685,10 @@ cast _ _ _ = Procedure (messageIO "Error: Invalid Cast")
                                                           
                                                                           
 
-
+--was replaced by changeZoneAction with the resolve trigger only working if the card is on the Stack.
 counter :: Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 counter = undefined
+
 
                   --From    To   
 changeZoneAction :: Zone -> Zone -> Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
@@ -627,23 +729,27 @@ affectDamage _ (Right p) (Just d) = let ac = changeLife d in
 affectDamage _ _ _                = Procedure (messageIO "Error: No value while changing life")
 
 
+--wasn't really necessary so wasn't implemented.
 affectTapUntap :: Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 affectTapUntap = undefined
 
-
+--would be used to apply temporary changes.
 affectChange :: Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 affectChange = undefined
 
-
+--doesn't really make sense without GUI
 reveal :: Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 reveal = undefined
 
-
+--activated abilities weren't fully implemented was still debating if I could fully code them into triggers.
 activateAbility :: Source -> (Either [CardId] [Player]) -> (Maybe Int) -> Procedure
 activateAbility = undefined
 
 
-                                      
+{--
+Functions that would specify how a game anturally processes and ask the players if they would want to take actions specific to the phase.
+Wasn't planned in the pitch to fully add. If this was fleshed out you could have game in the console.
+--}
 game :: IO ()
 game = do g <- initalize
           (while (not.checkWinner) turn) g
